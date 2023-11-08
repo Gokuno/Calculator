7488a15563c4335ba9c47e575e0c3b8b225df10b
Allan Paz <64302319+Gokuno@users.noreply.github.com>
2023-11-07 19:59:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//Functions and Operations
+
+function handleInteractions(value) {
+    console.log(value);
+}
+
+function handleNumericInput() {
+
+}
+
+function handleOperatorInput() {
+
+}
+
+function handleReset() {
+
+}
+
+function handleUpdateUI() {
+
+}
+
+function handleEquals() {
+
+}
